f9a0c33d78737cdb7960f8baf21a9e5f9f37860c
Moulshree Guleria <45145667+mollyx09@users.noreply.github.com>
2026-06-12 06:46:58 -0500

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # Brute Force solution - TC = O(n*n), SC = O(n)
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if (nums[i] == nums[j]):
+        #             return True
+        # return False
+
+        # #Better solution - used hashmap - what is the different b/w hash set and hash map and when should each be used
+        # numsDict = {}
+        # for i in range(len(nums)):
+        #     if nums[i] in numsDict:
+        #         return True
+        #     else:
+        #         numsDict[nums[i]] = i
+            
+        # return False
+
+        #Hashset approach
+        nums_set = set()
+        for n in nums:
+            if n in nums_set:
+                return True
+            nums_set.add(n)
+
+        return False
+
+                
