4839b1c04cf047239962a1bf6644c665bd383647
Moulshree Guleria <45145667+mollyx09@users.noreply.github.com>
2026-06-22 11:26:27 -0500

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        #Brute Force Approach/Inefficient sliding window approach
+        # l,r = 0,0
+        # counter = {}
+        # res = 0
+
+        # while r < len(s):
+        #     counter.clear()
+        #     substr = s [l:r + 1]
+        #     for ch in substr:
+        #         counter[ch] = 1 + counter.get(ch, 0)
+
+        #     max_key, max_value = max(counter.items(), key=lambda item: item[1])
+
+        #     if (len(substr) - max_value) <= k:
+        #         res = max((len(substr)), res)
+        #         r += 1
+        #     else:
+        #         l += 1
+            
+        # return res
+        
+        l = 0
+        res = 0
+        counter = {}
+        maxF = 0
+
+        for r in range(len(s)):
+            counter[s[r]] = 1 + counter.get(s[r], 0)
+            maxF = max(maxF, counter[s[r]])
+
+            while (r - l + 1) - maxF > k:
+                counter[s[l]] -= 1
+                l += 1
+            
+            res = max(res, r - l + 1)
+        
+        return res
